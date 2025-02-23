2a69f7e8e6997adf10e45b5f792b293db139ef81
Vishal Adithya.A <vishaladithya2008@gmail.com>
2025-02-23 17:27:22 +0530

    Compressed Preprocessing steps onto function

--- main.py
+++ main.py
@@ -22,28 +22,25 @@ df.dropna(how = "any",inplace = True)
 
 print([df["Brand"].unique(),df["Material"].unique(),df["Size"].unique(),df["Style"].unique(),df["Color"].unique()])
 
-brands = df["Brand"].map({"Jansport":0,"Under Armour":1,"Nike":2,"Adidas":3,"Puma":4})
-df["Brand"] = brands
+def Preprocessing(df):
+    brands = df["Brand"].map({"Jansport":0,"Under Armour":1,"Nike":2,"Adidas":3,"Puma":4})
+    mats = df["Material"].map({"Leather":0,"Canvas":1,"Nylon":2,"Polyester":3})
+    size = df["Size"].map({"Small":0,"Medium":1,"Large":2})
+    
+    df["Laptop Compartment"] = df["Laptop Compartment"].map(lambda x:0 if x=="No" else 1)
+    df["Waterproof"] = df["Waterproof"].map(lambda x:0 if x=="No" else 1)
+    
+    color = pd.get_dummies(df["Color"],prefix = "Color_",dtype=int)
+    style = pd.get_dummies(df["Style"],prefix = "Style_",dtype=int)
+    
+    df["Brand"] = brands
+    df["Material"] = mats
+    df["Size"] = size
+    
+    df = df.join(color)
+    df = df.join(style)
+    df.drop(columns = ["Color","Style"],inplace = True)
+    
+    return df
 
-
-mats = df["Material"].map({"Leather":0,"Canvas":1,"Nylon":2,"Polyester":3})
-df["Material"] = mats
-
-
-size = df["Size"].map({"Small":0,"Medium":1,"Large":2})
-df["Size"] = size
-
-
-df["Laptop Compartment"] = df["Laptop Compartment"].map(lambda x:0 if x=="No" else 1)
-df["Waterproof"] = df["Waterproof"].map(lambda x:0 if x=="No" else 1)
-
-
-color = pd.get_dummies(df["Color"],prefix = "Color_",dtype=int)
-style = pd.get_dummies(df["Style"],prefix = "Style_",dtype=int)
-df = df.join(color)
-df = df.join(style)
-df.drop(columns = ["Color","Style"],inplace = True)
-
-def Preprocessing(dataframe):
-    pass
-
+preprocessed_df = Preprocessing(df=df)
