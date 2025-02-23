e16ec342f13ad45f7b237e3f25a2e3cb933bf1cd
Vishal Adithya.A <vishaladithya2008@gmail.com>
2025-02-23 17:23:53 +0530

    Preprocessed all features

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ df.set_index("id",inplace = True)
 df.isnull().sum()
 df.dropna(how = "any",inplace = True)
 
+print([df["Brand"].unique(),df["Material"].unique(),df["Size"].unique(),df["Style"].unique(),df["Color"].unique()])
 
 brands = df["Brand"].map({"Jansport":0,"Under Armour":1,"Nike":2,"Adidas":3,"Puma":4})
 df["Brand"] = brands
@@ -33,15 +34,16 @@ size = df["Size"].map({"Small":0,"Medium":1,"Large":2})
 df["Size"] = size
 
 
+df["Laptop Compartment"] = df["Laptop Compartment"].map(lambda x:0 if x=="No" else 1)
+df["Waterproof"] = df["Waterproof"].map(lambda x:0 if x=="No" else 1)
 
-df["Brand"].unique()
-df["Material"].unique()
-df["Size"].unique()
-df["Style"].unique()
-df["Color"].unique()
 
+color = pd.get_dummies(df["Color"],prefix = "Color_",dtype=int)
+style = pd.get_dummies(df["Style"],prefix = "Style_",dtype=int)
+df = df.join(color)
+df = df.join(style)
+df.drop(columns = ["Color","Style"],inplace = True)
 
 def Preprocessing(dataframe):
-    df = dataframe
-    
+    pass
 
